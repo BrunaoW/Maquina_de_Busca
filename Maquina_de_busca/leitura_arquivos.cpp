051961e086eba9e442efa2279f997edf27241171
Bruno Wilson <brunowilson4@gmail.com>
2019-06-04 09:21:26 -0300

    Comentarios adicionados

--- Maquina_de_busca/leitura_arquivos.cpp
+++ Maquina_de_busca/leitura_arquivos.cpp
@@ -7,20 +7,26 @@ list<Documento> LeituraArquivos::LerArquivosDaPastaAtual() {
 
 	int contador;
 	DIR* diretorioAtual = NULL;
+
+	// Abre a pasta onde se encontra todos os arquivos a serem lidos
 	diretorioAtual = opendir("dataset_teste");
 
 	dirent* arquivo = NULL;
 
+	// Verifica se a pasta foi aberta corretamente
 	if (diretorioAtual == NULL) {
 		cout << "Diretorio nao iniciado corretamente." << endl;
 		exit(3);
 	}
 
+	// le todos os arquivos presentes na pasta de dataset
 	while (arquivo = readdir(diretorioAtual)) {
 		if (arquivo == NULL) {
 			cout << "Arquivo nao iniciado corretamente." << endl;
 			exit(3);
 		}
+
+		// filtra os arquivos com o nome "." e "..", que sao as pastas referentes a atual e a anterior
 		if (strcmp(arquivo->d_name, ".") != 0 && strcmp(arquivo->d_name, "..") != 0)
 			documentos.push_back(Documento(arquivo->d_name));
 	}
@@ -31,7 +37,6 @@ list<Documento> LeituraArquivos::LerArquivosDaPastaAtual() {
 
 void LeituraArquivos::LerPalavrasDeDocumentos(list<Documento>& documentos, IndiceInvertido& indiceInvertido) {
 	for (Documento& documento : documentos) {
-		
 		ifstream arquivo;
 		arquivo.open("dataset_teste/" + documento.ObterNome());
 		string palavra;
@@ -46,7 +51,11 @@ void LeituraArquivos::LerPalavrasDeDocumentos(list<Documento>& documentos, Indic
 }
 
 void LeituraArquivos::AdicionarPalavraAoIndiceInvertido(Documento& documento, IndiceInvertido& IndiceInvertido, Palavra palavra) {
+	// Ao mesmo tempo que busca os documentos associados, a palavra ja eh inclusa no indice invertido
 	set<Documento>& documentosAssociados = IndiceInvertido.ObterRegistros()[palavra];
+	
+	// Verifica se o documento lido se apresenta na lista de documentos ja associados a palavra. Se nao, insere o documento no registro
+	//    do indice invertido associado a palavra
 	if (documentosAssociados.find(documento) == documentosAssociados.end()) {
 		documentosAssociados.insert(documento);
 	}
@@ -54,6 +63,8 @@ void LeituraArquivos::AdicionarPalavraAoIndiceInvertido(Documento& documento, In
 
 void LeituraArquivos::AdicionarPalavraAoDocumento(Documento& documento, Palavra palavra) {
 	map<Palavra, int>& palavras = documento.ObterPalavras();
+
+	// Ao mesmo tempo que inclui a palavra na lista de palavras no documento, ja incrementa a contagem de quantas vezes ela apareceu
 	palavras[palavra]++;
 }
 
@@ -61,6 +72,7 @@ void LeituraArquivos::NormalizarPalavra(string& palavra) {
 	string palavraNormalizada;
 
 	for (auto& caracter : palavra) {
+		// Verifica se existe alguma pontuacao separando a palavra, se nao, adiciona o caracter na palavra
 		if (!ispunct((unsigned char)caracter))
 		{
 			palavraNormalizada += caracter;
